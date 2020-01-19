b27c0605763da1c7f214a91bb447af3a68896c37
Gerhard Molin <gary@Gerhards-iMac.home>
2020-01-19 20:13:34 +0200

    other

---
+++ level04/other/rev_wstr.c
@@ -0,0 +1,65 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rev_wstr.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gmolin <gmolin@student.hive.fi>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/12/08 17:04:37 by gmolin            #+#    #+#             */
+/*   Updated: 2020/01/05 17:34:10 by gmolin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdlib.h>
+
+void    ft_putstr(char *str)
+{
+    int i = 0;
+
+    while (str[i])
+        write(1, &str[i++], 1);
+}
+
+
+char *rev_wstr(char *str)
+{
+    int len;
+    int i;
+    int pos;
+    char *rev_str;
+
+    while (str[len])
+        len++;
+    rev_str = malloc(sizeof(char) * len + 1);
+    len--;
+    i = 0;
+    while (len >= 0)
+    {
+        while (str[len] > 32)
+            len--;
+        pos = len;
+        len++;
+        while (str[len] > 32 && str[len])
+            rev_str[i++] = str[len++];
+        len = pos;
+        if (str[len] == ' ' || str[len] == '\t' || str[len] == '\n')
+            rev_str[i++] = str[len];
+        len--;
+    }
+    rev_str[i] = '\0';
+    return (rev_str);
+}
+
+int main(int argc, char **argv)
+{
+    char *str;
+
+    if (argc == 2)
+    {
+        str = rev_wstr(argv[1]);
+        ft_putstr(str);
+    }
+    write(1, "\n", 1);
+    return (0);
+}
---
+++ level04/other/sort_int_tab.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sort_int_tab.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gmolin <gmolin@student.hive.fi>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/11/22 00:59:02 by jaleman           #+#    #+#             */
+/*   Updated: 2020/01/07 10:10:07 by gmolin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	sort_int_tab(int *tab, unsigned int size)
+{
+	int				tmp;
+	unsigned int 	i;
+	unsigned int 	j;
+
+	i = 0;
+	while (i < size - 1)
+	{
+		j = i;
+		while (j < size)
+		{
+			if (tab[i] > tab[j])
+			{
+				tmp = tab[i];
+				tab[i] = tab[j];
+				tab[j] = tmp;
+			}
+			j += 1;
+		}
+		i += 1;
+	}
+}
